a080e4a1c74f6471e8b85acd56089bb86bfb9127
Angel1841 <angelatanasov10@gmail.com>
2022-04-23 22:56:12 +0300

    uploaded GuineiaPig.java

---
+++ Java Exams/MidExamPreparation/GuineiaPig.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class GuineiaPig {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double food = Double.parseDouble(scanner.nextLine());
+        double hay = Double.parseDouble(scanner.nextLine());
+        double cover = Double.parseDouble(scanner.nextLine());
+
+        double weight = Double.parseDouble(scanner.nextLine());
+
+        food = food * 1000;
+        hay *= 1000;
+        cover *= 1000;
+        weight *= 1000;
+
+        for (int i = 1; i <= 30; i++) {
+
+            food -= 300;
+
+            if(i % 2 == 0){
+
+                hay -= (food * 0.05);
+
+
+            }
+
+            if(i % 3 == 0){
+
+                cover -= weight * 1/3;
+
+            }
+
+        }
+
+        if(food>=0 && hay>=0 && cover >= 0){
+            System.out.printf("Everything is fine! Puppy is happy! Food: %.2f, Hay: %.2f, Cover: %.2f.",food / 1000,hay / 1000,cover / 1000);
+        } else{
+            System.out.println("Merry must go to the pet store!");
+        }
+    }
+}
