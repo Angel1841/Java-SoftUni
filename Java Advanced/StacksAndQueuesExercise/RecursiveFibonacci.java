a6c434e6d3ae47cfff89ef4c643d489e7c91b7d5
Angel1841 <angelatanasov10@gmail.com>
2022-05-20 18:22:23 +0300

    uploaded RecursiveFibonacci.java

---
+++ Java Advanced/StacksAndQueuesExercise/RecursiveFibonacci.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class RecursiveFibonacci {
+
+    public static long[] memory;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        memory = new long[n + 1];
+
+        if(n == 0){
+            System.out.println(0);
+            return;
+        }
+
+        long result = fib(n);
+
+
+
+        System.out.println(result);
+    }
+
+    private static long fib(int n){
+
+        if(n < 2){
+            return 1;
+        }
+
+        if(memory[n] != 0){
+            return memory[n];
+        }
+
+        memory[n] = fib(n - 1) + fib( n - 2);
+
+        return memory[n];
+
+    }
+}
