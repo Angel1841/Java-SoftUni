b323df0f7d41c44cb9aa7dc9b631749330160fab
Angel1841 <angelatanasov10@gmail.com>
2022-06-19 23:10:29 +0300

    uploaded LIstOfPredicates.java

---
+++ Java Advanced/FunctionalProgrammingExercise/LIstOfPredicates.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class LIstOfPredicates {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        Predicate<Integer> isDivisible =  m -> {
+
+            for (Integer number : numbers) {
+                if (m % number != 0) {
+                    return false;
+                }
+            }
+            return true;
+        };
+
+        for (int i = 1; i <= n; i++) {
+            if(isDivisible.test(i)){
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+}
