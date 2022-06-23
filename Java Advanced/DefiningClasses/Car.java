a0a406cee623cd75c8e70a06eae902ef500b6274
Angel1841 <angelatanasov10@gmail.com>
2022-06-24 00:59:29 +0300

    uploaded Car.java

---
+++ Java Advanced/DefiningClasses/Car.java
@@ -0,0 +1,49 @@
+public class Car {
+
+    private String brand;
+    private String model;
+    private int horsePower;
+
+    public Car(String brand, String model, int horsePower) {
+        this.brand = brand;
+        this.model = model;
+        this.horsePower = horsePower;
+    }
+
+    public Car(String brand) {
+        this(brand, "unknown", -1);
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String carInfo(){
+
+        return String.format("The car is: %s %s - %d HP.",
+                getBrand(),
+                getModel(),
+                getHorsePower());
+
+    }
+}
