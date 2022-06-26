23afbe2bd011a2c849da5a890eadfbd981628c01
Angel <angelatanasov10@gmail.com>
2022-06-26 14:15:00 +0300

    uploaded Exercise.java

---
+++ Java Advanced/Exam25June2022/workout/Exercise.java
@@ -0,0 +1,29 @@
+package workout;
+
+public class Exercise {
+    private String name;
+    private String muscle;
+    private int burnedCalories;
+
+    public Exercise(String name, String muscle, int burnedCalories) {
+        this.name = name;
+        this.muscle = muscle;
+        this.burnedCalories = burnedCalories;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMuscle() {
+        return muscle;
+    }
+    public int getBurnedCalories() {
+        return burnedCalories;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Exercise: %s, %s, %d", this.name, this.muscle, this.burnedCalories);
+    }
+}
