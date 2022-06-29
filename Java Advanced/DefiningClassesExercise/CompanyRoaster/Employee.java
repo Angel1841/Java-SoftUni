0ffe865d320144ef4e5952bda7c5a61b5e6e5537
Angel <angelatanasov10@gmail.com>
2022-06-29 21:28:43 +0300

    uploaded Employee.java

---
+++ Java Advanced/DefiningClassesExercise/CompanyRoaster/Employee.java
@@ -0,0 +1,44 @@
+package CompanyRoster;
+
+public class Employee {
+
+    private String name;
+    private double salary;
+    private String position;
+    private String department;
+    private String email;
+    private int age;
+
+    public Employee(String name, double salary, String position, String department, String email, int age) {
+       this(name, salary, position, department, email);
+        this.age = age;
+    }
+
+    public Employee(String name, double salary, String position, String department) {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+        this.email = "n/a";
+        this.age = -1;
+    }
+
+    public Employee(String name, double salary, String position, String department, String email) {
+        this(name, salary, position, department);
+        this.email = email;
+    }
+
+    public Employee(String name, double salary, String position, String department, int age) {
+        this(name, salary, position, department);
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %.2f %s %d",this.name, this.salary, this.email, this.age);
+    }
+}
