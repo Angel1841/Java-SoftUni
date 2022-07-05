f30f0cebc2b9709eb9b3110c1d17dae237292271
Angel <angelatanasov10@gmail.com>
2022-07-05 18:32:44 +0300

    uploaded Book.java

---
+++ Java Advanced/IteratorsAndComparators/ComparableBook/Book.java
@@ -0,0 +1,54 @@
+package ComparableBook;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Book implements Comparable<Book> {
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        setTitle(title);
+        setYear(year);
+        setAuthors(authors);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String... authors) {
+        this.authors = Arrays.asList(authors);
+    }
+
+    public int compareTo(Book other) {
+        int result = this.title.compareTo(other.title);
+
+        if(result == 0){
+            result = Integer.compare(this.year, other.year);
+        }
+
+        return result;
+    }
+
+    
+
+
+}
