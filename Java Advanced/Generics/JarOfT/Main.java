aff1604cbe4321f853339a1438a03373647d4cc0
Angel <angelatanasov10@gmail.com>
2022-07-08 18:33:24 +0300

    uploaded Main.java

---
+++ Java Advanced/Generics/JarOfT/Main.java
@@ -0,0 +1,12 @@
+package JarOfT;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Jar<Integer> jar = new Jar<>();
+
+        jar.add(13);
+        jar.remove();
+
+    }
+}
