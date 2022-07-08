0d8c2583234b35e09bac655e6520578aee652c08
Angel <angelatanasov10@gmail.com>
2022-07-08 18:33:52 +0300

    uploaded Jar.java

---
+++ Java Advanced/Generics/JarOfT/Jar.java
@@ -0,0 +1,21 @@
+package JarOfT;
+
+import java.util.ArrayDeque;
+
+public class Jar<E> {
+
+    private ArrayDeque<E> elements;
+
+    public Jar() {
+        this.elements = new ArrayDeque<>();
+    }
+
+    public void add(E element){
+        elements.push(element);
+    }
+
+    public E remove(){
+        return elements.pop();
+    }
+
+}
