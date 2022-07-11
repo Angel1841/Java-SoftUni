3bdf4d93bd9c4d888bbbd801ce03baf03df2ce74
Angel <angelatanasov10@gmail.com>
2022-07-12 00:04:40 +0300

    uploaded CustomList

---
+++ Java Advanced/GenericsExercise/CustomList/CustomList.java
@@ -0,0 +1,55 @@
+package CustomList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CustomList<T extends Comparable<T>> {
+
+    List<T> elements;
+
+    public CustomList() {
+        elements = new ArrayList<>();
+    }
+
+    public void add(T element) {
+        elements.add(element);
+    }
+
+
+    public T remove(int index) {
+        return elements.remove(index);
+    }
+
+    public boolean contains(T element) {
+        return elements.contains(element);
+    }
+
+
+    public void swap(int index, int index1) {
+        Collections.swap(elements, index, index1);
+    }
+
+    public int countGreaterThan(T value) {
+        return (int) elements.stream().filter(e -> e.compareTo(value) > 0).count();
+    }
+
+    public T getMax() {
+        return elements.stream().max(Comparable::compareTo).get();
+    }
+
+    public T getMin() {
+        return elements.stream().min(Comparable::compareTo).get();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (T t : elements) {
+            sb.append(t).append(System.lineSeparator());
+        }
+        return sb.toString();
+    }
+}
+
+
