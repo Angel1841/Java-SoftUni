30794b3cca3f8ecee1a7d912f695b1c9e1e0ad47
Angel <angelatanasov10@gmail.com>
2022-07-13 23:40:32 +0300

    uploaded Main.java

---
+++ Java Advanced/GenericsExercise/GenericCountMethodString/Main.java
@@ -0,0 +1,18 @@
+package GenericCountMethodString;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        Box<String>box = new Box<>();
+        while (n-- > 0) {
+            box.add(scanner.nextLine());
+        }
+
+        String value = scanner.nextLine();
+        System.out.println(box.countGreater(value));
+    }
+
+}
