2dd46c9c766100f8bc5e2d3959ee1712b21b45d7
Angel <angelatanasov10@gmail.com>
2022-07-13 23:38:03 +0300

    uploaded GenericBox

---
+++ Java Advanced/GenericsExercise/GenericBox/Box.java
@@ -0,0 +1,16 @@
+package GenericBox;
+
+public class Box <T>{
+
+    T element;
+
+    public Box(T element) {
+        this.element = element;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s%n", element.getClass().getName(), element);
+    }
+
+}
