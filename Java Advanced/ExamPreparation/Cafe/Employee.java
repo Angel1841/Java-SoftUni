797d12e7ee6b066656c64673ac7f90f03341f754
Angel <angelatanasov10@gmail.com>
2022-07-18 20:14:07 +0300

    uploaded Cafe Employee

---
+++ Java Advanced/ExamPreparation/Cafe/Employee.java
@@ -0,0 +1,26 @@
+package cafe;
+
+public class Employee {
+    private String name;
+    private int age;
+    private String country;
+
+    public Employee(String name, int age, String country) {
+        this.name = name;
+        this.age = age;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Employee: %s, %d from %s", this.name, this.age, this.country);
+    }
+}
