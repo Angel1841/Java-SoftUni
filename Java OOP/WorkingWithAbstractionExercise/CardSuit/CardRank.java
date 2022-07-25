375c5be0a9724f60cf5ef65ef4f9df064626fb14
Angel <angelatanasov10@gmail.com>
2022-07-25 23:49:09 +0300

    uploaded CardRank.java

---
+++ Java OOP/WorkingWithAbstractionExercise/CardSuit/CardRank.java
@@ -0,0 +1,19 @@
+package CardSuit;
+
+public enum CardRank {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING;
+
+
+    }
