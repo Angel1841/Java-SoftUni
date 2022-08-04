3972e61a99ca24f7444e4b38722451106a316ffd
Angel <angelatanasov10@gmail.com>
2022-08-04 21:11:16 +0300

    uploaded Main.java

---
+++ Java OOP/ReflectionExercise/barracksWars/Main.java
@@ -0,0 +1,21 @@
+package barracksWars;
+
+import barracksWars.interfaces.Repository;
+import barracksWars.interfaces.Runnable;
+import barracksWars.interfaces.UnitFactory;
+import barracksWars.core.Engine;
+import barracksWars.core.factories.UnitFactoryImpl;
+import barracksWars.data.UnitRepository;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+
+    public static void main(String[] args) throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        Repository repository = new UnitRepository();
+        UnitFactory unitFactory = new UnitFactoryImpl();
+
+        Runnable engine = new Engine(repository, unitFactory);
+        engine.run();
+    }
+}
