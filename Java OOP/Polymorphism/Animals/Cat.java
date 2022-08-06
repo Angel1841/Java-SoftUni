f443774f6f304f76bc36fe8b4369a5a59ec47098
Angel <angelatanasov10@gmail.com>
2022-08-07 00:54:57 +0300

    uploaded Cat.java

---
+++ Java OOP/Polymorphism/Animals/Cat.java
@@ -0,0 +1,12 @@
+package Animals;
+
+public class Cat extends Animal {
+    public Cat(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return super.explainSelf() + System.lineSeparator() + "MEEOW";
+    }
+}
