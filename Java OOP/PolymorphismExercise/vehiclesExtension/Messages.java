2044094888803af3081f2db43288adb231e95e2f
Angel <angelatanasov10@gmail.com>
2022-08-12 23:57:39 +0300

    uploaded Messages.java

---
+++ Java OOP/PolymorphismExercise/vehiclesExtension/Messages.java
@@ -0,0 +1,8 @@
+package vehiclesExtension;
+
+public class Messages {
+    public static final String TRAVELING = "%s travelled %s km";
+    public static final String NOT_TRAVELING = "%s needs refueling";
+    public static final String INVALID_FUEL_LITTERS = "Fuel must be a positive number";
+    public static final String FUEL_OVERLOADING = "Cannot fit fuel in tank";
+}
