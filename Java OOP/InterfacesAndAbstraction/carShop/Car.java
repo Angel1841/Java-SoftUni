1f50d3c680e9f7652e4a0bc2570755dca5d396a2
Angel <angelatanasov10@gmail.com>
2022-08-14 23:41:09 +0300

    uploaded cars

---
+++ Java OOP/InterfacesAndAbstraction/carShop/Car.java
@@ -0,0 +1,13 @@
+package carShop;
+
+import java.io.Serializable;
+
+public interface Car extends Serializable {
+    int TIRES = 4;
+
+    String getModel();
+    String getColor();
+    Integer getHorsePower();
+
+    String countryProduced();
+}
---
+++ Java OOP/InterfacesAndAbstraction/carShop/CarImpl.java
@@ -0,0 +1,40 @@
+package carShop;
+
+public class CarImpl implements Car{
+
+    private String model;
+    private String color;
+    private Integer horsePower;
+    private String countryProduced;
+
+    public CarImpl(String model, String color, Integer horsePower, String countryProduced) {
+        this.model = model;
+        this.color = color;
+        this.horsePower = horsePower;
+        this.countryProduced = countryProduced;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("This is %s produced in %s and have %d tires%n", model, countryProduced, TIRES);
+    }
+
+    @Override
+    public String getModel() {
+        return model;
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public Integer getHorsePower() {
+        return horsePower;
+    }
+
+    public String countryProduced() {
+        return countryProduced;
+    }
+}
