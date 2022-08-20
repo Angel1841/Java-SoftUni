e050a107db7e887b3ea3f8032f1e16084ac244fc
Angel <angelatanasov10@gmail.com>
2022-08-20 21:08:54 +0300

    uploaded Ferrari and Main

---
+++ Java OOP/InterfacesAndAbstraction/ferrari/Main.java
@@ -0,0 +1,17 @@
+package ferrari;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String driverName = scanner.nextLine();
+
+        Car car = new Ferrari(driverName);
+
+        System.out.println(car.toString());
+
+    }
+}
