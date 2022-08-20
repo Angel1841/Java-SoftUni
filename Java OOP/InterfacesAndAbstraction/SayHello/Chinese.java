da14e5842284bb47db4e8d2d3fe7b80935ab2eea
Angel <angelatanasov10@gmail.com>
2022-08-20 21:10:06 +0300

    uploaded Chinese.java and European.java

---
+++ Java OOP/InterfacesAndAbstraction/SayHello/Chinese.java
@@ -0,0 +1,13 @@
+package SayHello;
+
+public class Chinese extends BasePerson {
+
+    public Chinese(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Djydjybydjy";
+    }
+}
