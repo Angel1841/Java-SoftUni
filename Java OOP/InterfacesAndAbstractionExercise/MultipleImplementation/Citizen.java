3fe274f068261f179d0e545a995cb24c458a9db2
Angel <angelatanasov10@gmail.com>
2022-08-30 18:28:36 +0300

    uploaded MultipleImplementation

---
+++ Java OOP/InterfacesAndAbstractionExercise/MultipleImplementation/Citizen.java
@@ -0,0 +1,45 @@
+package MultipleImplementation;
+
+public class Citizen implements Person, Identifiable, Birthable {
+    private String name;
+    private int age;
+    private String id;
+    private String birthDate;
+
+    public Citizen(String name, int age, String id, String birthDate) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Citizen{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", id='" + id + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                '}';
+    }
+}
