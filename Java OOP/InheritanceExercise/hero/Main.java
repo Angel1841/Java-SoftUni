27df71cdc40978104b3c390b2d0f3e21ff15b5bd
Angel <angelatanasov10@gmail.com>
2022-09-04 22:35:58 +0300

    uploaded hero

---
+++ Java OOP/InheritanceExercise/hero/Main.java
@@ -0,0 +1,9 @@
+package hero;
+
+public class Main {
+    public static void main(String[] args) {
+        Hero hero = new Hero("petar",12);
+
+        System.out.println(hero.toString());
+    }
+}
