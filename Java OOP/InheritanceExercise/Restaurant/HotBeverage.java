d9cde6f63f2c451063d78274b8320abf7892eee2
Angel <angelatanasov10@gmail.com>
2022-09-05 23:36:07 +0300

    uploaded Restaurant

---
+++ Java OOP/InheritanceExercise/Restaurant/HotBeverage.java
@@ -0,0 +1,12 @@
+package Restaurant;
+
+import java.math.BigDecimal;
+
+public class HotBeverage extends Beverage{
+
+    public HotBeverage(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+
+
+}
