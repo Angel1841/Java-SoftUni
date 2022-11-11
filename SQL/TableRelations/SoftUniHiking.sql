b943f795f84f4b0cf3f0e233b787c1871a29d74d
Angel <angelatanasov10@gmail.com>
2022-11-11 20:40:41 +0200

    uploaded SoftUniHiking.sql

---
+++ SQL/TableRelations/SoftUniHiking.sql
@@ -0,0 +1,10 @@
+SELECT 
+    r.starting_point AS 'route_starting_point',
+    r.end_point AS 'route_ending_point',
+    r.leader_id,
+    CONCAT(c.first_name, ' ', c.last_name) AS 'leader_name'
+FROM
+    `routes` AS r
+        JOIN
+    `campers` AS c ON r.leader_id = c.id; 
+
